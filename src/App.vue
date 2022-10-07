3efcaeaa9fbab8235e89ffa5580a0c6aef4fc32e
albert <xsoularisx@gmail.com>
2022-10-07 19:16:30 +0300

    init: start my personal costs

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <img alt="Vue logo" src="./assets/logo.png">
   </div>
 </template>
 
@@ -15,6 +11,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
 
 nav {
